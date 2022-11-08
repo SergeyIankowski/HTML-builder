fd00067a333029447c8f4ddbe88d19ece75d38d1
SeregeyIankowski <garret933@gmail.com>
2022-11-08 16:13:53 +0300

    feat: add infinite writing in 2 task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,9 +5,13 @@ const fs = require("fs");
 stdout.write("write your text below\n");
 stdin.on("data", data => {
 
+  if (data.toString("utf-8").slice(0, 4) === "exit") {
+    console.log("\nyou will exit, good bye!");
+    exit();
+  }
+
   fs.readFile(path.resolve(__dirname, "text.txt"), "utf-8", (error, content) => {
     if (error) console.error("not file in the directory, create new text.txt file");
-
     let fileContent = content;
     if (fileContent === undefined) {
       fileContent = data;
@@ -16,14 +20,12 @@ stdin.on("data", data => {
     }
     fs.writeFile(path.resolve(__dirname, "text.txt"), fileContent, err => {
       if (err) console.error(err.message);
-      console.log("text is written in file");
-      exit();
     });
   });
 
 });
 
 process.on("SIGINT", () => {
-  stdout.write("\ngood bye!\n");
+  console.log("\ngood bye!");
   exit();
 });
